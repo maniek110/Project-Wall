d1a5e741c40e5d62523385ba0114dd333a46b478
Suchy <marcinsuchora@gmail.com>
2019-08-12 00:07:13 +0200

    Correctly displayed pages

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  List<int> pages=List.generate(5, (index) => index,growable: true);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(body: SafeArea(
+      child: PageView.builder(
+        itemCount: pages.length,
+      itemBuilder: (context,index){
+          return Container(
+            height: double.infinity,
+            width: double.minPositive,
+            color: randomColor,
+            margin: const EdgeInsets.all(5),
+          );
+      },
+    )
+    )
+    );
+  }
+
+  Color get randomColor=>
+      Color((Random().nextDouble()*0xFFFFFF).toInt()<<0).withOpacity(1);
+}
+class PageScrollPhysics extends ScrollPhysics{
+  const PageScrollPhysics({ScrollPhysics parent}):super(parent:parent);
+  PageScrollPhysics apply(ScrollPhysics ancestor){
+    return PageScrollPhysics(parent: buildParent(ancestor));
+  }
+}
+
